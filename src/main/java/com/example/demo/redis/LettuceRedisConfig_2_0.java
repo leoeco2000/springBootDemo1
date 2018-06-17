ad44e2574c41361bf4e17ef0ae201e1c2efc3642
leoeco2000 <leoeco2000@qq.com>
2018-06-18 02:46:22 +0800

    redisson

--- src/main/java/com/example/demo/redis/LettuceRedisConfig_2_0.java
+++ src/main/java/com/example/demo/redis/LettuceRedisConfig_2_0.java
@@ -2,15 +2,7 @@ package com.example.demo.redis;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Configuration
 public class LettuceRedisConfig_2_0 {
---
+++ src/main/java/com/example/demo/redis/RedsissonConfig.java
@@ -0,0 +1,21 @@
+package com.example.demo.redis;
+
+import java.io.IOException;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+@Configuration
+@ComponentScan
+public class RedsissonConfig {
+    @Bean(destroyMethod="shutdown")
+    RedissonClient redisson(@Value("classpath:/conf/redisson.yaml") Resource configFile) throws IOException {
+        Config config = Config.fromYAML(configFile.getInputStream());
+        return Redisson.create(config);
+    }
+}
