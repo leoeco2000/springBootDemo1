c993cc669ae611e029e5ecc236af05245e8c1892
leoeco2000 <leoeco2000@qq.com>
2018-09-04 02:18:55 +0800

    boot

---
+++ src/test/java/com/example/demo/TestCache.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.test.context.junit4.SpringRunner;
+import com.example.demo.service.BookRepository;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@EnableCaching
+public class TestCache{
+
+    private static final Logger logger = LoggerFactory.getLogger(TestCache.class);
+
+    @Autowired
+    BookRepository bookRepository;
+
+    public TestCache() {
+    }
+
+    @Test
+    public void testFind() throws Exception {
+        logger.info(".... Fetching books");
+        logger.info("isbn-1234 -->" + bookRepository.getByIsbn("isbn-1234"));
+        logger.info("isbn-4567 -->" + bookRepository.getByIsbn("isbn-4567"));
+        logger.info("isbn-1234 -->" + bookRepository.getByIsbn("isbn-1234"));
+        logger.info("isbn-4567 -->" + bookRepository.getByIsbn("isbn-4567"));
+        logger.info("isbn-1234 -->" + bookRepository.getByIsbn("isbn-1234"));
+        logger.info("isbn-1234 -->" + bookRepository.getByIsbn("isbn-1234"));
+    }
+
+}
